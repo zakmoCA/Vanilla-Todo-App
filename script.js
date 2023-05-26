843ea3ea3772adede1da2d85e92a6f544c83112c
zakmoCA <13905@coderacademy.edu.au>
2023-05-26 18:50:37 +1000

    'add task' functionality and task component finished

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const taskInput = document.getElementById('taskinput')
 const addBtn = document.getElementById('add')
 const taskContainer = document.getElementById('task')
 const deleteBtn = document.getElementById('delete')
+let taskDiv = document.getElementById('taskdiv')
+
+function createTodo() {
+  const task = taskInput.value // store user input value
+  if (task !== '') { // create task if there is input
+    // dynamically create the 'task' element
+    const newTask = document.createElement("li")
+    // we will want delete functionality so each task comes with delete button
+    const deleteButton = document.createElement("button")
+    const individualTaskDiv = document.createElement('div')
+    // set unique id for each 'task'
+    individualTaskDiv.setAttribute('id', `task-${Date.now()}`);
+    individualTaskDiv.setAttribute('class', 'individual')
+    deleteButton.setAttribute('id', 'delete')
+    deleteButton.innerText = 'Delete'
+    newTask.innerText = task
+    taskContainer.appendChild(individualTaskDiv)
+    individualTaskDiv.appendChild(newTask)
+    individualTaskDiv.appendChild(deleteButton)
+    taskInput.value = ''
+  }
+}
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  createTodo()
+})
